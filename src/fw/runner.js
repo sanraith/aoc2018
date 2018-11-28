d0764056ccba5775fc3f2ff506dc8bfd1061c13b
Soma Zsják <zsjaks@gmail.com>
2018-11-28 22:44:26 +0100

    Minor refactor.

--- src/fw/runner.js
+++ src/fw/runner.js
@@ -14,21 +14,21 @@ async function run(selectedDay) {
     files.forEach(fileName => debug(`- ${fileName}`));
 
     // Import solution class from the last file
-    let [selectedFile] = files.slice(-1);
+    let selectedFile = files.slice(-1)[0];
     if (Number.isInteger(parseInt(selectedDay, 10))) {
         const regex = /day(?:([0-9]{2}))\.js/;
-        const tempFile = files.filter(x => parseInt(regex.exec(x)[1], 10) === selectedDay)[0];
+        const tempFile = files.filter(f => parseInt(regex.exec(f)[1], 10) === selectedDay)[0];
         selectedFile = tempFile !== undefined ? tempFile : selectedFile;
     }
 
     debug(`Selected: ${selectedFile}`);
     const fileToImport = path.join(puzzleDir, selectedFile);
     // eslint-disable-next-line global-require, import/no-dynamic-require
-    const SolutionImplementation = require(fileToImport);
+    const SolutionDayXX = require(fileToImport);
 
     // Run solution
     /** @type { Solution } */
-    const solution = new SolutionImplementation();
+    const solution = new SolutionDayXX();
     await solution.init();
     debug(`Part 1: ${solution.part1()}`);
     debug(`Part 2: ${solution.part2()}`);
