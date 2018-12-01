673748a54d6bdd9327a399e2cebd591a82731f27
sanraith <zsjaks@gmail.com>
2018-12-01 09:55:46 +0100

    Added day 2 template.

---
+++ src/puzzles/day02.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+const debug = require('debug')('aoc.puzzles.day02');
+const Solution = require('../fw/solution');
+
+class Day02 extends Solution {
+    constructor() {
+        super(2, 'TODO');
+    }
+
+    // part1() {
+    //     return undefined;
+    // }
+
+    // part2() {
+    //     return undefined;
+    // }
+}
+
+module.exports = Day02;
