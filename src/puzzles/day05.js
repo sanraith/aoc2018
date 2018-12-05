0add04236b18dcf882c2bfaeff781cf49b33c4cb
sanraith <zsjaks@gmail.com>
2018-12-05 14:25:18 +0100

    Improved day 5 solution.

--- src/puzzles/day05.js
+++ src/puzzles/day05.js
@@ -6,75 +6,59 @@ class Day05 extends Solution {
     constructor() { super(5, 'Alchemical Reduction'); }
 
     part1() {
-        let [a, b] = this.process(this.input[0]);
-        let length = a.length;
-        do {
-            this.progress(1 - length / this.input[0].length);
-            length = a.length;
-            for (let i = 0; i < length - 1; i++) {
-                if (a[i] === b[i + 1]) {
-                    a[i] = '';
-                    a[i + 1] = '';
-                    b[i] = '';
-                    b[i + 1] = '';
-                }
-            }
-
-            a = this.clean(a);
-            b = this.clean(b);
-        } while (a.length < length);
-
-        return a.length;
+        return this.reduce([...this.input[0]]);
     }
 
     part2() {
-        let minlength = this.input[0].length;
-        let minChars;
+        const input = [...this.input[0]];
+        let minlength = input.length;
         for (let charCode = 65; charCode < 91; charCode++) {
-            let [a, b] = this.process(this.input[0]);
+            this.progress(charCode, 91, 65);
             const charsToRemove = [String.fromCharCode(charCode), String.fromCharCode(charCode + 32)];
-            a = Array.from(a.filter(c => !charsToRemove.includes(c)));
-            b = Array.from(b.filter(c => !charsToRemove.includes(c)));
-
-            let length = a.length;
-            do {
-                this.progress(charCode + (1 - length / this.input[0].length), 91, 65);
-
-                length = a.length;
-                for (let i = 0; i < length - 1; i++) {
-                    if (a[i] === b[i + 1]) {
-                        a[i] = '';
-                        a[i + 1] = '';
-                        b[i] = '';
-                        b[i + 1] = '';
-                    }
-                }
-
-                a = this.clean(a);
-                b = this.clean(b);
-            } while (a.length < length);
+            const length = this.reduce(input.filter(x => !charsToRemove.includes(x)));
 
             if (length < minlength) {
                 minlength = length;
-                minChars = charsToRemove;
-                debug(minlength, minChars);
             }
         }
 
         return minlength;
     }
 
-    clean(a) {
-        return Array.from(a.filter(c => c !== ''));
+    reduce(input) {
+        const polimer = input;
+        const inverse = Array.from(polimer.map(c => (c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase())));
+
+        const check = [0, 1];
+        while (check[0] !== undefined && check[1] !== undefined) {
+            if (polimer[check[0]] === inverse[check[1]]) {
+                polimer[check[0]] = '_';
+                polimer[check[1]] = '_';
+                check[0] = this.findNextUnit(polimer, check[0], -1);
+                check[1] = this.findNextUnit(polimer, check[1], 1);
+            } else {
+                check[0] = undefined;
+            }
+
+            if (check[0] === undefined) {
+                check[0] = this.findNextUnit(polimer, check[1] - 1, 1);
+                check[1] = this.findNextUnit(polimer, check[0], 1);
+            }
+        }
+
+        return polimer.filter(x => x !== '_').join('').length;
     }
 
-    /**
-     * @param { String } s
-     */
-    process(s) {
-        const a = Array.from(s);
-        const inverse = Array.from(a.map(c => (c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase())));
-        return [a, inverse];
+    findNextUnit(polimer, start, direction) {
+        let pos = start + direction;
+        while (pos >= 0 && pos < polimer.length && polimer[pos] === '_') {
+            pos += direction;
+        }
+        if (pos < 0 || pos >= polimer.length) {
+            return undefined;
+        }
+
+        return pos;
     }
 }
 
