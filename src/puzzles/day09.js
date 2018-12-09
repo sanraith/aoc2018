a5bb6d2394a94ece4ce9b2d32e2a70443e0ec6cd
Soma Zsják <zsjaks@gmail.com>
2018-12-09 13:59:34 +0100

    Improved day 9 solution. Added day 10 template.

--- src/puzzles/day09.js
+++ src/puzzles/day09.js
@@ -2,66 +2,89 @@
 const debug = require('debug')('aoc.puzzles.day09');
 const Solution = require('../fw/solution');
 
+class Linked {
+    /**
+     * @param { any } value
+     * @param { Linked } head
+     * @param { Linked } tail
+     */
+    constructor(value, head = this, tail = this) {
+        this.head = head;
+        this.tail = tail;
+        this.value = value;
+    }
+
+    delete() {
+        this.head.tail = this.tail;
+        this.tail.head = this.head;
+        return this.tail;
+    }
+
+    insert(value) {
+        const link = new Linked(value, this.head, this);
+        this.head.tail = link;
+        this.head = link;
+        return link;
+    }
+
+    /** @param { Number } steps */
+    step(steps) {
+        const isForward = steps > 0;
+        let remaining = Math.abs(steps);
+        let current = this;
+        while (remaining > 0) {
+            current = isForward ? current.tail : current.head;
+            remaining--;
+        }
+        return current;
+    }
+
+    values() {
+        let current = this;
+        const values = [];
+        do {
+            values.push(current.value);
+            current = current.tail;
+        } while (current !== this);
+        return values;
+    }
+}
+
 class Day09 extends Solution {
     constructor() { super(9, 'Marble Mania'); }
 
     part1() {
-        let { playerCount, lastMarble } = this.parseInput();
-        const circle = [0];
-        const players = new Map();
-        let current = 1;
-        let index = 0;
-        let playerIndex = 0;
-
-        // playerCount = 13;
-        // lastMarble = 7999;
-        while (current <= lastMarble) {
-            if (current % 23 === 0) {
-                index = (7 * circle.length + index - 7) % circle.length;
-                const extraPoint = circle[index];
-                circle.splice(index, 1);
-                if (!players.has(playerIndex)) { players.set(playerIndex, 0); }
-                players.set(playerIndex, players.get(playerIndex) + current + extraPoint);
-            } else {
-                index = ((index + 1) % circle.length) + 1;
-                circle.splice(index, 0, current);
-            }
-            current++;
-            playerIndex = (playerIndex + 1) % playerCount;
-        }
-
-        const highest = Array.from(players.values()).reduce((acc, x) => x > acc ? x : acc, 0);
-        return highest;
+        const { playerCount, lastMarble } = this.parseInput();
+        return this.solve(playerCount, lastMarble);
     }
 
     part2() {
-        let { playerCount, lastMarble } = this.parseInput();
-        const circle = [0];
-        const players = new Map();
-        let current = 1;
-        let index = 0;
-        let playerIndex = 0;
+        const { playerCount, lastMarble } = this.parseInput();
+        return this.solve(playerCount, lastMarble * 100);
+    }
 
-        // playerCount = 13;
-        lastMarble *= 100;
-        while (current <= lastMarble) {
-            this.progress(current, lastMarble);
-            if (current % 23 === 0) {
-                index = (7 * circle.length + index - 7) % circle.length;
-                const extraPoint = circle[index];
-                circle.splice(index, 1);
-                if (!players.has(playerIndex)) { players.set(playerIndex, 0); }
-                players.set(playerIndex, players.get(playerIndex) + current + extraPoint);
+    solve(playerCount, lastMarble) {
+        const players = new Map(); for (let i = 0; i < playerCount; i++) { players.set(i, 0); }
+        let circle = new Linked(0);
+        let marble = 1;
+        let player = 0;
+
+        while (marble <= lastMarble) {
+            if (marble % 23 === 0) {
+                circle = circle.step(-7);
+                players.set(player, players.get(player) + marble + circle.value);
+                circle = circle.delete();
             } else {
-                index = ((index + 1) % circle.length) + 1;
-                circle.splice(index, 0, current);
+                circle = circle.step(2);
+                circle = circle.insert(marble);
             }
-            current++;
-            playerIndex = (playerIndex + 1) % playerCount;
+            marble++;
+            player = (player + 1) % playerCount;
+            this.progress(marble, lastMarble);
         }
 
-        const highest = Array.from(players.values()).reduce((acc, x) => x > acc ? x : acc, 0);
-        return highest;
+        const max = Array.from(players.values()).reduce((acc, x) => x > acc ? x : acc, 0);
+        return max;
     }
 
     parseInput() {
---
+++ src/puzzles/day10.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+const debug = require('debug')('aoc.puzzles.day10');
+const Solution = require('../fw/solution');
+
+class Day10 extends Solution {
+    constructor() { super(10, 'TODO'); }
+
+    part1() {
+
+    }
+
+    part2() {
+
+    }
+}
+
+module.exports = Day10;
